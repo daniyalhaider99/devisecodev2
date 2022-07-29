c64de1605be4559e8b428bd461ea3b9be86eda96
daniyalhaider99 <109658940+daniyalhaider99@users.noreply.github.com>
2022-07-29 11:45:20 +0500

    Update home_pages_controller.rb

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,32 +1,4 @@
 class HomePagesController < ApplicationController
   def index
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-    define_singleton_methodd
-
-
-
   end
 end
